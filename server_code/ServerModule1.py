e479876410c6dceb877079d9e4bf09995051b66b
arhodes011235+anvil@gmail.com <arhodes011235+anvil@gmail.com>
2023-11-03 16:50:21 +0000

    Edited ServerModule1 and Survey

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -6,7 +6,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-
+import datetime
+    
 @anvil.server.callable
 def check_admin():
     if anvil.users.get_user():
@@ -14,82 +15,59 @@ def check_admin():
 
 @anvil.server.callable
 @anvil.tables.in_transaction
-def add_msr_check(
-    qa_name,
-    shift,
-    product_size,
-    length,
-    board_1_moisture,
-    board_1_moe,
-    board_1_fractured,
-    board_1_fb_value,
-    board_2_moisture,
-    board_2_moe,
-    board_2_fractured,
-    board_2_fb_value,
-    board_3_moisture,
-    board_3_moe,
-    board_3_fractured,
-    board_3_fb_value,
-    board_4_moisture,
-    board_4_moe,
-    board_4_fractured,
-    board_4_fb_value,
-    board_5_moisture,
-    board_5_moe,
-    board_5_fractured,
-    board_5_fb_value,
-    comments,
-):
-    shift_row = app_tables.shift.get(option=shift)  # Assuming 'shift' is the table name and 'option' is the column name
-    shift_row['num_checks'] = (shift_row['num_checks'] or 0) + 1
+def add_msr_check(qa_name, shift, product_size, length, board_data, comments):
+    # First, add board details to the `boards` table
+    board_entries = []
+    for board in board_data:
+        # Assuming `board` is a dictionary with keys: 'moisture', 'moe', 'fractured', 'fb_value'
+        fractured = (board['fractured'] == "Yes")
+        board_entry = app_tables.boards.add_row(
+            moisture=board['moisture'],
+            moe=board['moe'],
+            fractured=fractured,
+            fb_value=board['fb_value'] if fractured else None
+        )
+        board_entries.append(board_entry)
+    timestamp = datetime.datetime.now(datetime.timezone.utc)
+    
+    shift_row = app_tables.shift.get(option=shift)  # Assuming 'shifts' is the table name and 'option' is the column name
+    if shift_row:
+        shift_row['num_checks'] = (shift_row['num_checks'] or 0) + 1
 
-    product_size_row = app_tables.product_size.get(option=product_size)  # Adjust the table and column name accordingly
-    product_size_row['num_checks'] = (product_size_row['num_checks'] or 0) + 1
+    product_size_row = app_tables.product_size.get(option=product_size)
+    if product_size_row:
+        product_size_row['num_checks'] = (product_size_row['num_checks'] or 0) + 1
 
-    length_row = app_tables.length.get(option=length)  # Adjust the table and column name accordingly
-    length_row['num_checks'] = (length_row['num_checks'] or 0) + 1
-        
-    app_tables.msr_checks.add_row(
+    length_row = app_tables.length.get(option=length)
+    if length_row:
+        length_row['num_checks'] = (length_row['num_checks'] or 0) + 1
+
+    # Next, add the check to the `checks` table with references to the board entries
+    check_entry = app_tables.msr_checks.add_row(
+        check_completed_datetime=timestamp,
         qa_name=qa_name,
         shift=shift_row,
         product_size=product_size_row,
         length=length_row,
-        board_1_moisture=board_1_moisture,
-        board_1_moe=board_1_moe,
-        board_1_fractured=board_1_fractured,
-        board_1_fb_value=board_1_fb_value,
-        board_2_moisture=board_2_moisture,
-        board_2_moe=board_2_moe,
-        board_2_fractured=board_2_fractured,
-        board_2_fb_value=board_2_fb_value,
-        board_3_moisture=board_3_moisture,
-        board_3_moe=board_3_moe,
-        board_3_fractured=board_3_fractured,
-        board_3_fb_value=board_3_fb_value,
-        board_4_moisture=board_4_moisture,
-        board_4_moe=board_4_moe,
-        board_4_fractured=board_4_fractured,
-        board_4_fb_value=board_4_fb_value,
-        board_5_moisture=board_5_moisture,
-        board_5_moe=board_5_moe,
-        board_5_fractured=board_5_fractured,
-        board_5_fb_value=board_5_fb_value,
+        board_1=board_entries[0],
+        board_2=board_entries[1],
+        board_3=board_entries[2],
+        board_4=board_entries[3],
+        board_5=board_entries[4],
         comments=comments
     )
-
+    return check_entry
 
 @anvil.server.callable
 def get_total_responses():
-    return len(app_tables.responses.search())
+    return len(app_tables.msr_checks.search())
 
 @anvil.server.callable
 def get_tables():
-    ages = app_tables.age.search()
-    frequency = app_tables.frequency.search()
-    methods = app_tables.method.search()
-    ratings = app_tables.ratings.search()
-    return [ages, frequency, methods, ratings]
+    shift = app_tables.shift.search()
+    product_size = app_tables.product_size.search()
+    length = app_tables.length.search()
+    return [shift, product_size, length]
 
 @anvil.server.background_task
 def hello_world():
